e1df0d3d34cbda8a8648bb9b73a61c79a7979806
SaarChaffee <saarchaffee@qq.com>
2023-12-13 21:48:10 +0800

    fix: format when json.dump

--- to_json.py
+++ to_json.py
@@ -2,10 +2,10 @@ import json
 import _pcr_data as p
 
 with open('chara_name.json', 'w', encoding='utf-8') as f:
-    json.dump(p.CHARA_NAME, f, ensure_ascii=False)
+    json.dump(p.CHARA_NAME, f, ensure_ascii=False, indent=2)
 
 with open('unavailable_chara.json', 'w', encoding='utf-8') as f:
-    json.dump(list(p.UnavailableChara), f, ensure_ascii=False)
+    json.dump(list(p.UnavailableChara), f, ensure_ascii=False, indent=2)
 
 with open('chara_profile.json', 'w', encoding='utf-8') as f:
-    json.dump(p.CHARA_PROFILE, f, ensure_ascii=False)
+    json.dump(p.CHARA_PROFILE, f, ensure_ascii=False, indent=2)
