e0c992f7ca049f2b5b670ac310c513abc7dc532b
SaarChaffee <saarchaffee@qq.com>
2023-12-13 17:42:07 +0800

    feat: sync to json using github action

---
+++ to_json.py
@@ -0,0 +1,11 @@
+import json
+import _pcr_data as p
+
+with open('chara_name.json', 'w', encoding='utf-8') as f:
+    json.dump(p.CHARA_NAME, f, ensure_ascii=False)
+
+with open('unavailable_chara.json', 'w', encoding='utf-8') as f:
+    json.dump(list(p.UnavailableChara), f, ensure_ascii=False)
+
+with open('chara_profile.json', 'w', encoding='utf-8') as f:
+    json.dump(p.CHARA_PROFILE, f, ensure_ascii=False)
